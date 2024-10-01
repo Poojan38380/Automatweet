1636626a7bd26bb129ff45e3f302f18a1c1b998d
Poojan <poojangoyani@gmail.com>
2024-10-01 16:03:53 +0530

    added more wildcard tweets

--- gpt/generate_prompt.py
+++ gpt/generate_prompt.py
@@ -19,12 +19,49 @@ wildcards = [
     "talk about what you ate for dinner",
     "brag about how much you can bench press",
     "advocate for privacy",
-    "say you just changed your twitter banner"
+    "say you just changed your twitter banner",
+    "complain about slow Wi-Fi",
+    "declare your love for coffee or tea",
+    "confess your obsession with a random TV show",
+    "rant about modern art",
+    "say something controversial about pineapple on pizza",
+    "talk about how you've quit social media 10 times",
+    "give unsolicited life advice",
+    "pretend you're a tech startup founder",
+    "overhype a boring weekend plan",
+    "mock astrology or horoscopes",
+    "make fun of internet trends like TikTok dances",
+    "talk about 'adulting' being hard",
+    "talk about your 'genius' New Year's resolutions",
+    "pretend you're running for president",
+    "announce you're starting a podcast",
+    "declare your love for a random historical figure",
+    "give a shoutout to your 'haters'"
+    "talk about how you woke up and chose violence",
+    "brag about staying in bed all day",
+    "declare war on Monday mornings",
+    "pretend you're writing a bestseller novel",
+    "mock people who use too many hashtags",
+    "complain about influencer culture",
+    "make fun of your morning routine",
+    "talk about how you're 'totally fine' (you're not)",
+    "discuss your complex relationship with carbs",
+    "talk about binge-watching a new show",
+    "give fake relationship advice",
+    "pretend you're an expert on something you're not",
+    "jokingly announce you're deleting the app",
+    "brag about your non-existent workout routine",
+    "joke about online dating disasters",
+    "declare that you're moving to Mars",
+    "rant about online shopping addictions",
+    "complain about getting no retweets",
+    "mock your obsession with collecting random facts",
+    "talk about being 'deeply misunderstood'"    
 ]
 
 def create_tweet_prompt(keywords=[]):
     # 70% chance to use a keyword or 30% to use a wildcard
-    use_keyword = random.random() < 0.7
+    use_keyword = random.random() < 0.6
 
     # Select random wildcard or keyword
     if use_keyword and keywords:
