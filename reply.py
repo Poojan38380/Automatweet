4797de198c748a9731c9caca095358373201ebd8
Poojan <poojangoyani@gmail.com>
2024-10-02 12:06:13 +0530

    Changed some function names and made the code more readable

--- reply.py
+++ reply.py
@@ -1,4 +1,4 @@
-from replybot.reply_elon import get_elon_tweets,get_single_elon_tweet
+from replybot.fetch_tweets_to_reply import get_elon_tweets,get_single_elon_tweet
 from replybot.reply_prompt import generate_gpt_prompt
 from gpt.gpt_client import use_gpt
 from bot.tweet_poster import reply_to_tweet
@@ -6,7 +6,7 @@ from accounts.select_accounts import select_twitter_account
 from accounts.get_credentials import get_twitter_account_details
 from bot.auth_provider import get_twitter_client
 
-def elon_main():
+def reply_main():
 
         username = select_twitter_account("Select account to post to:")
         credentials = get_twitter_account_details(username=username)
@@ -19,7 +19,7 @@ def elon_main():
         print(f"\n{posted_by} tweeted: {message_content}\n")
         prompt = generate_gpt_prompt(tweet=message_content)
         reply_tweet= use_gpt(prompt=prompt)
-        print(reply_tweet)
+        print(f"Reply message : {reply_tweet}")
 
         reply_to_tweet(client=client, tweet=reply_tweet, tweet_id=tweet_id)
 
